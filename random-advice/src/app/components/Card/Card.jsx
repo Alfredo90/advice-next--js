702a64789b1e19fc0c4a40cc6c41eb3b5f1a7262
Alfredo <kmstry214@gmail.com>
2024-01-09 21:18:25 -0600

    error handle api call

--- random-advice/src/app/components/Card/Card.jsx
+++ random-advice/src/app/components/Card/Card.jsx
@@ -1,30 +1,53 @@
-"use client"
+"use client";
 
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 
-import Advice from '../Advice/Advice';
+import Advice from "../Advice/Advice";
 
 const Card = () => {
-  const [advices, setAdvices] = useState({})
+  const [advices, setAdvices] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
-const handleClick = async () => {
-  try {
-      const res = await fetch('https://api.adviceslip.com/advice')
-      const data = await res.json()
-      setAdvices(data.slip)
-  } catch (err) {
-      console.log(err.message)
-  }
-}
+  // const handleClick = async () => {
+  //   try {
+  //       const res = await fetch('https://api.adviceslip.com/advice')
+  //       const data = await res.json()
+  //       setAdvices(data.slip)
+  //   } catch (err) {
+  //       console.log(err.message)
+  //   }
+  // }
 
-return (
-  <div className='border-2 flex flex-col justify-center items-center'>
+  const fetchAdvice = async () => {
+    try {
+      setIsLoading(true);
+      const res = await fetch("https://api.adviceslip.com/advice");
+      const data = await res.json();
+      setAdvices(data.slip);
+    } catch (err) {
+      console.error("Error fetching data", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleClick = async () => {
+    await fetchAdvice();
+    // const delay = 5000;
+    // setTimeout(() => {
+    //   handleClick();
+    // }, delay);
+  };
+
+  return (
+    <div className="border-2 flex flex-col justify-center items-center">
       <h2>ADVICE #{advices.id}</h2>
       <h1>"{advices.advice}"</h1>
 
-      <button  type="submit" onClick={handleClick} >Generate</button>
-
-  </div>
-)
-}
-export default Card
+      <button type="submit" onClick={handleClick} disabled={isLoading}>
+        {isLoading ? 'Loading' : 'Fetch Advice'}
+      </button>
+    </div>
+  );
+};
+export default Card;
