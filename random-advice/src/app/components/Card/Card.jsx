84b9ed56b62d2bd6db402b84c656af469ac0e592
Alfredo <kmstry214@gmail.com>
2024-01-09 19:52:48 -0600

    able to make function call api

--- random-advice/src/app/components/Card/Card.jsx
+++ random-advice/src/app/components/Card/Card.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+"use client"
+
+import {useEffect, useState} from 'react'
+
+import Advice from '../Advice/Advice';
 
 const Card = () => {
-  return (
-    <div>
-      Card Page
-    </div>
-  )
+  const [advices, setAdvices] = useState({})
+
+const handleClick = async () => {
+  try {
+      const res = await fetch('https://api.adviceslip.com/advice')
+      const data = await res.json()
+      setAdvices(data.slip)
+  } catch (err) {
+      console.log(err.message)
+  }
 }
 
+return (
+  <div className='border-2 flex flex-col justify-center items-center'>
+      <h2>ADVICE #{advices.id}</h2>
+      <h1>"{advices.advice}"</h1>
+
+      <button  type="submit" onClick={handleClick} >Generate</button>
+
+  </div>
+)
+}
 export default Card
